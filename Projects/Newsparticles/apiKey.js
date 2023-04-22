29263bf18004117f077972e28129623cfa26fcf6
nagarjun73 <nagarjun7397@gmail.com>
2023-04-22 10:40:18 +0530

    updates

--- Projects/Newsparticles/apiKey.js
+++
@@ -1,3 +0,0 @@
-const apiKey = "cbe9fa26f6424df5b56d2a45a19c3e9c";
-
-export default apiKey;
--- Projects/Newsparticles/src/js/components/side-cards.componenr.jsx
+++
@@ -1,8 +0,0 @@
-const sideCardlHtml = (arts, i) => {
-  return `<div class="cards p-3 border-bottom">
-              <h6 class="card-text">${arts.title}</h6>
-      <p class="card-text">By <i>${arts.author}</i></p>
-        </div>`;
-};
-
-export default sideCardlHtml;
